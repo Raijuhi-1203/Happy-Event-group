9893ce06501fe44f520262489e6db316985c45f4
Juhi Rai <juhi.digitalbull@gmail.com>
2026-07-21 10:22:49 +0530

    commit

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -11,18 +11,18 @@ public partial class MasterPage : System.Web.UI.MasterPage
     {
         logo_link.HRef = GetRouteUrl("Index", null);
         home_link.HRef = GetRouteUrl("Index", null);
-        home2_link.HRef = GetRouteUrl("Index", null);
+       // home2_link.HRef = GetRouteUrl("Index", null);
 
         about_link.HRef = GetRouteUrl("About", null);
-        about2_link.HRef = GetRouteUrl("About", null);
+       // about2_link.HRef = GetRouteUrl("About", null);
 
         service_link.HRef = GetRouteUrl("Service", null);
-        service2_link.HRef = GetRouteUrl("Service", null);
+       // service2_link.HRef = GetRouteUrl("Service", null);
 
-        gallery2_link.HRef = GetRouteUrl("Gallery", null);
+       // gallery2_link.HRef = GetRouteUrl("Gallery", null);
         gallery_link.HRef = GetRouteUrl("Gallery", null);
         
-        contact2_link.HRef = GetRouteUrl("Contact", null);
+       // contact2_link.HRef = GetRouteUrl("Contact", null);
         contact_link.HRef = GetRouteUrl("Contact", null);
 
         term_link.HRef = GetRouteUrl("Term", null);
